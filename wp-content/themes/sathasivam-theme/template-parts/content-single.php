7012e5228385ed1c065901f60bdc12bb04e79f7a
0xgthuva <thillaiambalavanargobal@gmail.com>
2026-07-10 17:09:21 +0500

    Update content-single.php
    removed the date meta, comments and author meta on the books category, and the all posts

--- wp-content/themes/sathasivam-theme/template-parts/content-single.php
+++ wp-content/themes/sathasivam-theme/template-parts/content-single.php
@@ -1,119 +1,87 @@
 <div class="col-md-12">
+    <!--blog single section start here-->
+    <article  id="post-<?php the_ID(); ?>" <?php post_class('blog-single'); ?>>
+        <?php if( has_post_thumbnail() ){ ?>
+        <figure>
+            <?php the_post_thumbnail( 'full' , array( 'class' => 'img-responsive') ) ?>
+        </figure>
+        <?php } ?>
+        <div class="blog-content">
+        <?php 
+        the_title( '<h1 class="entry-title">', '</h1>' );
+        ?>
+        <!--ul class="post-time">
+            <li><i class="ion-person"></i><span><?php esc_html_e('Post by: ','academix')?></span><?php the_author_posts_link(); ?></li>
+            <li><i class="ion-calendar"></i><span><?php the_time(get_option('date_format')); ?></span></li>
+            <li><i class="ion-chatbox"></i><?php comments_popup_link( esc_html__('No Comment','academix'), esc_html__('1 Comment','academix'), esc_html__('% Comments','academix'), ' ', esc_html__('Comments off','academix')); ?></li>     
+        </ul -->
+        
+        
+        <!--added the fix for hiding the books post meta -->
+                <?php if ( get_post_type() !== 'book' ) : ?>
+   <ul class="post-time">
+    <li>
+        <i class="ion-calendar"></i>
+        <span><?php the_time( get_option( 'date_format' ) ); ?></span>
+    </li>
+</ul>
+<?php endif; ?>
+        <?php the_content(); ?>
 
-	<!-- Blog single section start -->
-	<article
-		id="post-<?php the_ID(); ?>"
-		<?php post_class( 'blog-single' ); ?>
-	>
-		<?php if ( has_post_thumbnail() ) : ?>
-			<figure>
-				<?php
-				the_post_thumbnail(
-					'full',
-					array(
-						'class' => 'img-responsive',
-					)
-				);
-				?>
-			</figure>
-		<?php endif; ?>
+          <!--div class="blo-cat"><span><strong><?php esc_html_e('Categories: ','academix'); ?></strong><?php the_category(', '); ?></span></div-->
+           <?php if( has_tag() ){ ?>
+           <div class="blo-tag"><span><strong><?php esc_html_e('Tagged: ','academix'); ?></strong><?php the_tags('', ', '); ?></span></div>
+           <?php } ?>
 
-		<div class="blog-content">
-			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+           <?php 
+                wp_link_pages( array(
+                    'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'academix' ),
+                    'after'  => '</div>',
+                    'link_before' => '<span>',
+                    'link_after'  => '</span>',
+                ) );
+            ?>
 
-			<ul class="post-time">
-				<li>
-					<i class="ion-person"></i>
-					<span>
-						<?php esc_html_e( 'Post by: ', 'academix' ); ?>
-					</span>
-					<?php the_author_posts_link(); ?>
-				</li>
+        </div>
+    </article> <!-- /.blog-single -->
+    <!--blog single section end here-->
 
-				<li>
-					<i class="ion-calendar"></i>
-					<span>
-						<?php the_time( get_option( 'date_format' ) ); ?>
-					</span>
-				</li>
 
-				<li>
-					<i class="ion-chatbox"></i>
-					<?php
-					comments_popup_link(
-						esc_html__( 'No Comment', 'academix' ),
-						esc_html__( '1 Comment', 'academix' ),
-						esc_html__( '% Comments', 'academix' ),
-						' ',
-						esc_html__( 'Comments off', 'academix' )
-					);
-					?>
-				</li>
-			</ul>
+     <?php 
+        
+        $prev_post = get_previous_post();
+        $next_post = get_next_post();
 
-			<?php the_content(); ?>
+        if( $prev_post || $next_post ){
 
-			<div class="blo-cat">
-				<span>
-					<strong>
-						<?php esc_html_e( 'Categories: ', 'academix' ); ?>
-					</strong>
-					<?php the_category( ', ' ); ?>
-				</span>
-			</div>
+     ?>
+        <nav class="navigation post-navigation">
+            
 
-			<?php if ( has_tag() ) : ?>
-				<div class="blo-tag">
-					<span>
-						<strong>
-							<?php esc_html_e( 'Tagged: ', 'academix' ); ?>
-						</strong>
-						<?php the_tags( '', ', ' ); ?>
-					</span>
-				</div>
-			<?php endif; ?>
+            <div class="nav-links">
+                
+                <?php 
+                
+                previous_post_link('<div class="nav-previous">%link</div>', __( 'Previous post', 'academix') );
+                next_post_link('<div class="nav-next">%link</div>', __( 'Next post', 'academix') );
 
-			<?php
-			wp_link_pages(
-				array(
-					'before'      => '<div class="page-links">' . esc_html__( 'Pages:', 'academix' ),
-					'after'       => '</div>',
-					'link_before' => '<span>',
-					'link_after'  => '</span>',
-				)
-			);
-			?>
-		</div>
-	</article>
-	<!-- Blog single section end -->
+                ?>
 
-	<?php
-	$prev_post = get_previous_post();
-	$next_post = get_next_post();
-	?>
+            </div>
+        </nav>
+        
+    <?php } ?>
+    
+ 
+                            
+    <?php if ( comments_open() || '0' != get_comments_number() ) : ?>
 
-	<?php if ( $prev_post || $next_post ) : ?>
-		<nav class="navigation post-navigation">
-			<div class="nav-links">
-				<?php
-				previous_post_link(
-					'<div class="nav-previous">%link</div>',
-					__( 'Previous post', 'academix' )
-				);
-
-				next_post_link(
-					'<div class="nav-next">%link</div>',
-					__( 'Next post', 'academix' )
-				);
-				?>
-			</div>
-		</nav>
-	<?php endif; ?>
-
-	<?php if ( comments_open() || '0' !== get_comments_number() ) : ?>
-		<!-- Comment section start -->
-		<?php comments_template(); ?>
-		<!-- Comment section end -->
-	<?php endif; ?>
+    <!--comment section start here-->
+    
+    <?php comments_template(); ?>
+    
+    <!--comment section end here-->
+    
+    <?php endif; ?>
 
 </div>
